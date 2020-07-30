bc256b5f333ee518af368d9ff628cdc01e9480c0
MuthulakshmiSankaranarayanan <64149912+MuthulakshmiSankaranarayanan@users.noreply.github.com>
2020-07-30 09:41:14 -0500

    Delete ProductRepository.java
    Product Repository is no more required for Wishlist Microservice

--- ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.teqtron.wishlist;
-
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<ProductModel, String>{
-    
-}
